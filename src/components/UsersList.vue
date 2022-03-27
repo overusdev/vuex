d894553c620d9b05c039f05d7db40921ec62506e
Сергей Вилков <sergey.vilkov@rsv.ru>
2022-03-27 19:17:29 +0300

    UsersList component

---
+++ src/components/UsersList.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="users-output">
+    <h2
+        v-if="showTitle"
+        >
+        {{ usersTitle }}: {{ usersCount }}
+    </h2>
+    <div class="users-output__inner">
+      <div
+        v-for="item in users"
+        :key="item.username"
+        class="users-output__item"
+      >
+        <p class="users-output__text users-output__text--bold">{{ item.username }}</p>
+        <p class="users-output__text">{{ item.email }}</p>
+        <a
+          :href="item.url"
+          target="_blank"
+          class="users-output__text users-output__text--link"
+        >
+          {{ item.url }}
+        </a>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'UsersList',
+    props: {
+        users: {
+            type: Array,
+            require: true,
+            default: () => ([]),
+        },
+        usersCount: Number,
+        usersTitle: String,
+    },
+    data: () => ({
+
+    }),
+
+    computed: {
+        showTitle() {
+            return this.usersTitle && this.usersCount;
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/media';
+
+  .users-output {
+    background: #ccc;
+    min-height: 250px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @include on-desktop {
+      background: #fff;
+    }
+
+    &__inner {
+      @include on-desktop {
+        width: 340px;
+      }
+    }
+
+    &__item {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 26px 20px;
+      outline: 1px solid #ccc;
+      margin-bottom: 12px;
+    }
+
+    &__text {
+
+      &--bold {
+        font-weight: bold;
+      }
+
+      &--link {
+        display: inline-block;
+      }
+    }
+  }
+</style>
--- src/views/Api.vue
+++ src/views/Api.vue
@@ -1,28 +1,11 @@
 <template>
-  <div class="api-output">
-    <h2>Users count: {{ getAllUsersCount }}</h2>
-    <div class="api-output__inner">
-      <div
-        v-for="item in getAllUsers"
-        :key="item.username"
-        class="api-output__item"
-      >
-        <p class="api-output__text api-output__text--bold">{{ item.username }}</p>
-        <p class="api-output__text">{{ item.email }}</p>
-        <a
-          :href="item.url"
-          target="_blank"
-          class="api-output__text api-output__text--link"
-        >
-          {{ item.url }}
-        </a>
-
-      </div>
-    </div>
-  </div>
+  <UsersList
+    :users="getAllUsers"
+  />
 </template>
 
 <script>
+import UsersList from '@/components/UsersList';
 import { mapActions, mapGetters } from 'vuex';
 
 const usersActions = [
@@ -40,6 +23,10 @@ export default {
     return {};
   },
 
+  components: {
+    UsersList,
+  },
+
   async mounted() {
     await this.fetchUsers();
   },
@@ -55,41 +42,5 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/styles/media';
 
-  .api-output {
-    background: #ccc;
-    min-height: 250px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
 
-    @include on-desktop {
-      background: #fff;
-    }
-
-    &__inner {
-      @include on-desktop {
-        width: 340px;
-      }
-    }
-
-    &__item {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-      padding: 26px 20px;
-      outline: 1px solid #ccc;
-      margin-bottom: 12px;
-    }
-
-    &__text {
-
-      &--bold {
-        font-weight: bold;
-      }
-
-      &--link {
-        display: inline-block;
-      }
-    }
-  }
 </style>
