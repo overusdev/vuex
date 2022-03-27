009c3758d23a06c7b0bd06da2be73a0b78d73beb
Сергей Вилков <sergey.vilkov@rsv.ru>
2022-03-27 18:41:46 +0300

    Add files

---
+++ src/views/Api.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="api-output">
+
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+const usersActions = [
+  'fetchUsers',
+];
+
+const usersGetters = [
+  'getAllUsers',
+];
+
+export default {
+  name: "Api",
+  data() {
+    return {};
+  },
+
+  async mounted() {
+    await this.fetchUsers();
+  },
+  computed: {
+    ...mapGetters('users', usersGetters),
+  },
+  methods: {
+    ...mapActions('users', usersActions),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/media';
+
+  .api-output {
+
+    @include on-desktop {
+      height: 450px;
+      background: #000;
+    }
+  }
+</style>
